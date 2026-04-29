346eb4f2e317d892bb291dcaabb10f84120e1a60
Bishal paul <163500895+Bisha18@users.noreply.github.com>
2026-04-29 10:33:28 +0530

    Update API base URL to new endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dumbiq.onrender.com/api',
+  baseURL: 'https://dumb-ai-v77a.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
